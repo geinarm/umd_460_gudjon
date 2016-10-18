d26e5784dd2a3263f79c40dbc61fe3f0b83a3880
Gudjon Magnusson <gmagnusson@fc-md.umd.edu>
2016-10-17 22:14:26 -0400

    Implemented prob2

--- hw3/prob2.m
+++ hw3/prob2.m
@@ -1,4 +1,16 @@
 
-figure(’position’,get(0,’screensize’))
-axes(’position’,[0 0 1 1])
-[x,y] = ginput;
+%figure('position',get(0,'screensize'))
+%axes('position',[0 0 1 1])
+%[x,y] = ginput;
+
+H = csvread('hand.csv');
+x = H(:,1);
+y = H(:,2);
+
+n = length(x);
+s = (1:n)';
+t = (1:.05:n)';
+u = pchiptx(s,x,t);
+v = pchiptx(s,y,t);
+clf reset
+plot(x,y,'.',u,v,'-');
